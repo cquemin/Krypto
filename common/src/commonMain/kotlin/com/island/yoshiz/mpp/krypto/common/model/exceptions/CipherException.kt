bece910dc257fb3eb453cf429a83a1a7ca1bccdd
Cyrille Quemin <cyrille.quemin@gmail.com>
2019-10-05 16:45:10 +0100

    Add Common model: exceptions

---
+++ common/src/commonMain/kotlin/com/island/yoshiz/mpp/krypto/common/model/exceptions/CipherException.kt
@@ -0,0 +1,4 @@
+package com.island.yoshiz.mpp.krypto.common.model.exceptions
+
+class CipherException(message: String? = null, cause: Throwable? = null) :
+        KryptoException(message, cause)
---
+++ common/src/commonMain/kotlin/com/island/yoshiz/mpp/krypto/common/model/exceptions/SecureRandomException.kt
@@ -0,0 +1,4 @@
+package com.island.yoshiz.mpp.krypto.common.model.exceptions
+
+class SecureRandomException(message: String? = null, cause: Throwable? = null) :
+        KryptoException(message, cause)
